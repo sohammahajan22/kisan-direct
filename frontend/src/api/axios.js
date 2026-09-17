013b898c9e883743f4d142238db79ec25ebdd44a
Soham <mahajansoham201@gmail.com>
2026-09-17 23:54:16 +0530

    fix render allowed host

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://kisan-direct-yzn6.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
